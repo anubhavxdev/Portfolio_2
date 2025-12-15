d0725e202a7c697cd505768c45ffdb09bfd2ac2a
Anubhav Jaiswal <anubhav.jaiswal23@lpu.in>
2025-12-15 12:23:32 +0530

    snowfall effect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Sections/Hero"; 
+import Hero from "./components/Sections/Hero";
 import { BrowserRouter } from "react-router-dom";
 import Skills from "./components/Sections/Skills";
 import Experience from "./components/Sections/Experience";
 import Education from "./components/Sections/Eduaction";
-// import DogCat from "./components/canvas/Stars";
 import Contact from "./components/Sections/Contact";
 import Footer from "./components/Sections/Footer";
 import Sparkles from "./assets/sss";
-// import StarCanvas from "../canvas/Stars";
+import Snowfall from "react-snowfall";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   color: ${({ theme }) => theme.text_primary};
   width: 100%;
-  height: 90vh;
+  min-height: 100vh;
   overflow-x: hidden;
   position: relative;
 `;
@@ -42,19 +41,39 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
         <Navbar />
+
         <Body>
+          {/* ❄️ Snowfall Effect */}
+          <Snowfall
+            color="white"
+            snowflakeCount={120}
+            style={{
+              position: "fixed",
+              width: "100vw",
+              height: "100vh",
+              zIndex: 1,
+              pointerEvents: "none",
+            }}
+          />
+
+          {/* Optional Sparkle Layer */}
           <Sparkles />
-          <div>
-          <Hero />
-          <Wrapper>
-          <Skills />
-          <Experience />
-          </Wrapper>
-          <Wrapper>
-          <Education />
-          <Contact />
-          </Wrapper>
-          <Footer />
+
+          {/* Main Content */}
+          <div style={{ position: "relative", zIndex: 2 }}>
+            <Hero />
+
+            <Wrapper>
+              <Skills />
+              <Experience />
+            </Wrapper>
+
+            <Wrapper>
+              <Education />
+              <Contact />
+            </Wrapper>
+
+            <Footer />
           </div>
         </Body>
       </BrowserRouter>
